cf2bf47e5b19a69f7b02304617d48def4ff1a463
Derrick Strong <derrickmstrong@gmail.com>
2020-07-12 11:27:56 -0500

    Updates

---
+++ precache-manifest.bb7425cadb1e35ab74d02d475354d895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70827fe54b2fffb0e8e685f768ee2282",
+    "url": "/Project-MountWeather2.1/index.html"
+  },
+  {
+    "revision": "25b7babad44d36e35fa3",
+    "url": "/Project-MountWeather2.1/static/css/main.f62a9e6e.chunk.css"
+  },
+  {
+    "revision": "0571f9373869507bbb76",
+    "url": "/Project-MountWeather2.1/static/js/2.d2b142a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project-MountWeather2.1/static/js/2.d2b142a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b7babad44d36e35fa3",
+    "url": "/Project-MountWeather2.1/static/js/main.229bd558.chunk.js"
+  },
+  {
+    "revision": "a54718d1f70ab3858461",
+    "url": "/Project-MountWeather2.1/static/js/runtime-main.087ab19f.js"
+  }
+]);
